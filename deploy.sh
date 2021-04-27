984c6d2e3cf97e14f3095cf04c4dbbc8db2556bd
jan2705 <jan2705@outlook.de>
2021-04-27 16:39:42 +0200

    deploy: interactive mode

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ docker container stop tcg
 
 docker container rm tcg
 
-docker container run -d --name "tcg" -v /data/Pokemon-TCG:/app/data tcg_image
+docker container run -it --name "tcg" -v /data/Pokemon-TCG:/app/data tcg_image
 
 docker logs tcg

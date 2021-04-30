fce746b80a2be5373e399dd5629095b7204f0e83
jan2705 <jan2705@outlook.de>
2021-04-30 19:47:59 +0200

    test: app.js refactor

--- src/app.js
+++ src/app.js
@@ -1,172 +1,107 @@
-require('dotenv').config()
-const Discord = require('discord.js')
-const fs = require('fs')
-const prefix = require('./prefix.js')
-const language = require('./language.js');
-const user = require('./user.js')
-const help = require('./help.js')
-const deleteMessage = require('./deleteMessage.js')
+import {} from "dotenv/config";
+import fs from "fs";
+import Discord from "discord.js";
+import Statcord from "statcord.js";
+import prefix from "./prefix.js";
+import language from "./language.js";
+import user from "./user.js";
+import help from "./help.js";
+import deleteMessage from "./deleteMessage.js";
 
 const client = new Discord.Client();
-
-token = process.env.BOT_TOKEN
-
-// Create data folder if it doesn't exist
-if (!fs.existsSync('data/'))
-{
-    fs.mkdirSync('data/')
-}
-
-// Create server.json if it doesn't exist or is corrupted
-if (!fs.existsSync('data/server.json'))
-{
-    rawData = '{"servers":[]}'
-    fs.writeFileSync('data/server.json', rawData)
-}
-else
-{
-    rawData = fs.readFileSync('data/server.json')
-}
-try
-{
-    JSON.parse(rawData)
-}
-catch
-{
-    rawData = '{"servers":[]}'
-    fs.writeFileSync('data/server.json', rawData)
-}
-
-// Create user.json if it doesn't exist or is corrupted
-if (!fs.existsSync('data/user.json'))
-{
-    rawData = '{"users":[]}'
-    fs.writeFileSync('data/user.json', rawData)
-}
-else
-{
-    rawData = fs.readFileSync('data/user.json')
-}
-try
-{
-    JSON.parse(rawData)
-}
-catch
-{
-    rawData = '{"users":[]}'
-    fs.writeFileSync('data/user.json', rawData)
-}
-
-client.on('ready', () => 
-{
-    console.log(`app.js: Logged in as ${client.user.username}!`)
-    client.user.setActivity('ty help', {type: 'PLAYING'})
+const statcord = new Statcord.Client({
+    client,
+    key: process.env.STATS_KEY
 })
 
-client.on('message', msg => 
-{
+if (!fs.existsSync('data/')) {fs.mkdirSync('data/')}
+if (!fs.existsSync('data/server.json')) {let rawData = '{"servers":[]}'; fs.writeFileSync('data/server.json', rawData)} else {let rawData = fs.readFileSync('data/server.json')} try {JSON.parse(rawData)} catch {let rawData = '{"servers":[]}'; fs.writeFileSync('data/server.json', rawData)}
+if (!fs.existsSync('data/user.json')) {let rawData = '{"users":[]}'; fs.writeFileSync('data/user.json', rawData)} else {let rawData = fs.readFileSync('data/user.json')}try {JSON.parse(rawData)} catch {let rawData = '{"users":[]}'; fs.writeFileSync('data/user.json', rawData)}
+
+client.on('ready', () => {
+    console.log(`app.js: Logged in as ${client.user.username}!`)
+    client.user.setActivity('ty help', {type: 'PLAYING'})
+    statcord.autopost();
+})
+
+client.on('message', msg => {
     const author = msg.author
     const contentWithPrefix = msg.content.toLocaleLowerCase()
     const channel = msg.channel
      
     // Check if the bot is in a guild or in private message
-    if (msg.guild)
-    {
-        channelType = 'guild'
-        id = msg.guild.id
-    }
-    else
-    {
-        channelType = 'user'
-        id = msg.author.id
-    }
+    if (msg.guild) {let channelType = 'guild'; let id = msg.guild.id} else {let channelType = 'user'; let id = msg.author.id}
 
     const guildLanguage = language.get(id, channelType)
     const guildPrefix = prefix.get(id, channelType)
 
     // Check if message starts with prefix
-    if (contentWithPrefix.startsWith("ty "))
-    {
-        content = contentWithPrefix.substr(3).toLocaleLowerCase()
+    if (contentWithPrefix.startsWith("ty ")) {
+        let content = contentWithPrefix.substr(3).toLocaleLowerCase()
     }
-    else if (contentWithPrefix.startsWith(`${guildPrefix}`))
-    {
-        content = contentWithPrefix.substr(guildPrefix.length).toLocaleLowerCase()
+    else if (contentWithPrefix.startsWith(`${guildPrefix}`)) {
+        let content = contentWithPrefix.substr(guildPrefix.length).toLocaleLowerCase()
     }
-    else
-    {
-        return
-    }
+    else {return}
 
     // Language handling
-    if (content == "language list" && msg.member.hasPermission("ADMINISTRATOR"))
-    {
+    if (content == "language list" && msg.member.hasPermission("ADMINISTRATOR")) {
         language.list(channel)
         return
     }
-    else if (content.startsWith("language ") && msg.member.hasPermission("ADMINISTRATOR"))
-    {
+    else if (content.startsWith("language ") && msg.member.hasPermission("ADMINISTRATOR")) {
         language.set(content.substring(9), channel, id, channelType)
         return
     }
 
     // Prefix handling
-    if (content == "prefix" && msg.member.hasPermission("ADMINISTRATOR"))
-    {
+    if (content == "prefix" && msg.member.hasPermission("ADMINISTRATOR")) {
         prefix.show(guildPrefix, guildLanguage, channel)
         return
     }
-    else if (content.startsWith("prefix ") && msg.member.hasPermission("ADMINISTRATOR"))
-    {
+    else if (content.startsWith("prefix ") && msg.member.hasPermission("ADMINISTRATOR")) {
         prefix.set(id, channelType, content.substring(7), guildLanguage, channel)
         return
     }
 
     // Booster handling
-    if (content == "buy" || content == "b")
-    {
-        handler = new user.userHandler(author.id, channel, guildLanguage, author.id, id, channelType)
+    if (content == "buy" || content == "b") {
+        let handler = new user.userHandler(author.id, channel, guildLanguage, author.id, id, channelType)
         handler.buy(msg)
         return
     }
 
     // Money request handling
-    if (content == "money" || content == "m")
-    {
-        handler = new user.userHandler(author.id, channel, guildLanguage)
+    if (content == "money" || content == "m") {
+        let handler = new user.userHandler(author.id, channel, guildLanguage)
         handler.money(guildLanguage)
         return
     }
 
     // View request handling
-    if (content.startsWith("view") || content.startsWith("v"))
-    {
-        if (msg.mentions.users.first() != undefined)
-        {
-            handler = new user.userHandler(msg.mentions.users.first().id, channel, guildLanguage, author.id, id, channelType)
+    if (content.startsWith("view") || content.startsWith("v")) {
+        if (msg.mentions.users.first() != undefined) {
+            let handler = new user.userHandler(msg.mentions.users.first().id, channel, guildLanguage, author.id, id, channelType)
         }
-        else
-        {
-            handler = new user.userHandler(author.id, channel, guildLanguage, author.id, id, channelType)
+        else {
+            let handler = new user.userHandler(author.id, channel, guildLanguage, author.id, id, channelType)
         }
         handler.view(msg)
         return
     }
 
     // Delete message
-    if (content == "delete_message" && msg.member.hasPermission("ADMINISTRATOR"))
-    {
+    if (content == "delete_message" && msg.member.hasPermission("ADMINISTRATOR")) {
         deleteMessage.change(id, channelType, guildLanguage, channel)
         return
     }
 
     // Help
-    if (content == "help" || content == "h")
-    {
-        handler = new help.helpHandler(msg.member, channel, guildLanguage)
+    if (content == "help" || content == "h") {
+        let handler = new help.helpHandler(msg.member, channel, guildLanguage)
         handler.view()
         return
     }
 })
 
-client.login(token)
+client.login(process.env.BOT_TOKEN)
